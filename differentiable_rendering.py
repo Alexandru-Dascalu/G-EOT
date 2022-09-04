223b116e623e11ec59876a0565850a3cfabf747d
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2022-09-04 17:03:04 +0300

    creating photo error textures all at once now

--- differentiable_rendering.py
+++ differentiable_rendering.py
@@ -14,14 +14,7 @@ def render(textures, uv_mappings, print_error_params, photo_error_params, backgr
     Tensor of shape batch_size x 1000, representing the logits obtained by passing the adversarial images as
     input to the victim model.
     """
-    print_error_params = [(multiplier, addend) for multiplier, addend in zip(print_error_params[0], print_error_params[1])]
-    # create each image in batch from texture one at a time. We do this instead of all at once so that we need less
-    # memory (a 12 x 2048 x 2048 x 3 tensor is 600 MB, and we would create multiple ones). We make the first image
-    # outside of the loop to initialise the list of new images, and to avoid putting an if statement in the loop
-    new_images = create_image(textures[0], uv_mappings[0], print_error_params[0])
-    for i in range(1, cfg.batch_size):
-        image = create_image(textures[i], uv_mappings[i], print_error_params[i])
-        new_images = tf.concat([new_images, image], axis=0)
+    new_images = create_images(textures, uv_mappings, print_error_params)
 
     # add background colour to rendered images.
     new_images = add_background(new_images, uv_mappings, background_colour)
@@ -31,7 +24,6 @@ def render(textures, uv_mappings, print_error_params, photo_error_params, backgr
     # plt.imshow(new_images.numpy()[3])
     # plt.imshow(new_images.numpy()[4])
     # plt.imshow(new_images.numpy()[5])
-
 
     # check if we apply random noise to simulate camera noise
     if cfg.photo_error:
@@ -53,17 +45,17 @@ def render(textures, uv_mappings, print_error_params, photo_error_params, backgr
     return new_images
 
 
-def create_image(texture, uv_mapping, print_error_params):
+def create_images(textures, uv_mappings, print_error_params=None):
     """Create an image from the given texture using the given UV mapping.
 
     Parameters
     ----------
-    texture : Tensor
-        Tensor representing the 2048x2048x3 texture of this 3D model. Texture is must have pixel values between 0
-        and 255.
-    uv_mapping : numpy array
-        A numpy array with shape [image_height, image_width, 2]. Represents the UV mappings for an
-        image in the batch. This mappign is used to create the images from the textures.
+    textures : Tensor
+        Tensor representing the batch_size x 2048 x 2048 x 3 texture of this 3D model. Texture is must have 32-bit float
+        pixel values between 0 and 1.
+    uv_mappings : numpy array
+        A numpy array with shape batch_size x image_height x image_width x 2. Represents the UV mappings for an
+        image in the batch. This mapping is used to create the images from the textures.
 
     Returns
     -------
@@ -72,31 +64,17 @@ def create_image(texture, uv_mapping, print_error_params):
         renders with the normal texture. The second is of shape num_new_renders x 299 x 299 x 3, representing the images
         of the new renders with the adversarial texture.
     """
-    # check if we should add print errors, so that the adversarial texture may be used for a 3D printed object
+    # check if we should add print errors, so that the adversarial textures may be used for a 3D printed object
     # and still be effective
     if cfg.print_error:
         print_multiplier, print_addend = print_error_params
-        new_texture = transform(tf.identity(texture), print_multiplier, print_addend)
-        new_texture = tf.expand_dims(new_texture, axis=0)
-    else:
-        # tfa.resampler requires input to be in shape batch_size x height x width x channels, so we insert a new
-        # dimension
-        new_texture = tf.expand_dims(tf.identity(texture), axis=0)
-
-    # plt.imshow(new_texture.numpy()[0])
-    # plt.show()
-
-    # tfa.image.resampler requires the first dimension of UV map to be
-    # batch size, so we add an extra dimension with one element
-    image_uv_map = tf.expand_dims(uv_mapping, axis=0)
+        textures = transform(textures, print_multiplier, print_addend)
 
     # use UV mapping to create an images corresponding to an individual render by sampling from the texture
-    # Resulting tensors are of shape 1 x image_width x image_height x 3
-    new_image = tfa.image.resampler(new_texture, image_uv_map)
-    # plt.imshow(new_image.numpy()[0])
-    # plt.show()
+    # Resulting tensors are of shape batch_size x image_width x image_height x 3
+    new_images = tfa.image.resampler(textures, uv_mappings)
 
-    return new_image
+    return new_images
 
 
 def add_background(images, uv_mappings, background_colour):
