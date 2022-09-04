da199e08699b9a94fc3cdf23118b1c0ee5dfa3ed
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2022-09-04 18:38:21 +0300

    moved generator code

--- advnet.py
+++ advnet.py
@@ -9,44 +9,13 @@ import tensorflow as tf
 import numpy as np
 
 import data
-import layers
 import nets
-import encoders
 import differentiable_rendering as diff_rendering
-import preproc
+from generator import create_generator
 
 NoiseRange = 10.0
 cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits
 relu = tf.keras.activations.relu
-
-
-# input to generator must be textures with values normalised to -1 and 1
-def create_generator(num_experts):
-    textures = tf.keras.layers.Input(shape=(2048, 2048, 3), dtype=tf.float32)
-    targets = tf.keras.layers.Input(shape=(1,), dtype=tf.int64)
-    x = encoders.simpleNet_encoder(textures)
-
-    # add decoder part
-    x = layers.deconv2d_bn(x, filters=128, kernel_size=3, strides=2, activation=relu)
-    subnets = []
-    for idx in range(num_experts):
-        subnet = layers.deconv2d_bn(x, filters=64, kernel_size=3, strides=2, activation=relu)
-        subnet = layers.deconv2d_bn(subnet, filters=32, kernel_size=3, strides=2, activation=relu)
-        subnet = layers.deconv2d_bn(subnet, filters=16, kernel_size=3, strides=2, activation=relu)
-        subnet = layers.deconv2d_bn(subnet, filters=8, kernel_size=3, strides=2, activation=relu)
-        subnet = layers.deconv2d_bn(subnet, filters=3, kernel_size=3, strides=2, activation=relu)
-        subnets.append(tf.expand_dims(subnet, axis=-1))
-
-    subnets = tf.concat(subnets, axis=-1)
-    weights = tf.keras.layers.Dense(units=num_experts, use_bias=True,
-                                    kernel_regularizer=None,
-                                    activation=tf.keras.activations.softmax)(tf.one_hot(targets, 1000))
-
-    moe = tf.transpose(a=tf.transpose(a=subnets, perm=[1, 2, 3, 0, 4]) * weights, perm=[3, 0, 1, 2, 4])
-    noises = (tf.nn.tanh(tf.reduce_sum(input_tensor=moe, axis=-1)) - 0.5) * NoiseRange * 2
-    print('Shape of Noises: ', noises.shape)
-
-    return tf.keras.Model(inputs=[textures, targets], outputs=noises)
 
 
 hyper_params_imagenet = {'BatchSize': 6,
@@ -85,7 +54,7 @@ class AdvNet(nets.Net):
         self.adv_images = tf.zeros(shape=[self._hyper_params['BatchSize']] + self.image_shape + [3], dtype=tf.float32,
                                    name="adversarial images")
 
-        # define generator
+        # input to generator must be textures with values normalised to -1 and 1
         self.generator = create_generator(self._hyper_params['NumSubnets'])
         # define simulator
         self.simulator = self.create_simulator(architecture)
--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from layers import conv2d_bn, depthwise_conv2d_bn, sep_conv2d_bn
+from layers import conv2d_bn, depthwise_conv2d_bn, sep_conv2d_bn, deconv2d_bn
 
 relu = tf.keras.activations.relu
 
@@ -50,3 +50,30 @@ def simpleNet_encoder(textures):
     x = sep_conv2d_bn(x, filters=1024, kernel_size=3, activation=relu)
 
     return x
+
+def create_generator(num_experts):
+    textures = tf.keras.layers.Input(shape=(2048, 2048, 3), dtype=tf.float32)
+    targets = tf.keras.layers.Input(shape=(1,), dtype=tf.int64)
+    x = simpleNet_encoder(textures)
+
+    # add decoder part
+    x = deconv2d_bn(x, filters=128, kernel_size=3, strides=2, activation=relu)
+    subnets = []
+    for idx in range(num_experts):
+        subnet = deconv2d_bn(x, filters=64, kernel_size=3, strides=2, activation=relu)
+        subnet = deconv2d_bn(subnet, filters=32, kernel_size=3, strides=2, activation=relu)
+        subnet = deconv2d_bn(subnet, filters=16, kernel_size=3, strides=2, activation=relu)
+        subnet = deconv2d_bn(subnet, filters=8, kernel_size=3, strides=2, activation=relu)
+        subnet = deconv2d_bn(subnet, filters=3, kernel_size=3, strides=2, activation=relu)
+        subnets.append(tf.expand_dims(subnet, axis=-1))
+
+    subnets = tf.concat(subnets, axis=-1)
+    weights = tf.keras.layers.Dense(units=num_experts, use_bias=True,
+                                    kernel_regularizer=None,
+                                    activation=tf.keras.activations.softmax)(tf.one_hot(targets, 1000))
+
+    moe = tf.transpose(a=tf.transpose(a=subnets, perm=[1, 2, 3, 0, 4]) * weights, perm=[3, 0, 1, 2, 4])
+    noises = (tf.nn.tanh(tf.reduce_sum(input_tensor=moe, axis=-1)) - 0.5) * NoiseRange * 2
+    print('Shape of Noises: ', noises.shape)
+
+    return tf.keras.Model(inputs=[textures, targets], outputs=noises)
